6a1212a2e1e8bccc6a6dafaf0ac92e43b03fad60
Nate Beaty <nate@firebellydesign.com>
2018-10-22 15:26:34 -0500

    add get_template_part_with_vars

--- lib/fb-utils.php
+++ lib/fb-utils.php
@@ -80,3 +80,53 @@ function get_total_pages($category, $per_page) {
   return $num_pages;
 }
 
+/**
+ * Support for sending vars to get_template_part()
+ * e.g. \Firebelly\Utils\get_template_part_with_vars('templates/page', 'header', ['foo' => 'bar']);
+ * (from https://github.com/JolekPress/Get-Template-Part-With-Variables)
+ */
+function get_template_part_with_vars($slug, $name = null, array $namedVariables = []) {
+  // Taken from standard get_template_part function
+  \do_action("get_template_part_{$slug}", $slug, $name);
+
+  $templates = array();
+  $name = (string)$name;
+  if ('' !== $name)
+      $templates[] = "{$slug}-{$name}.php";
+
+  $templates[] = "{$slug}.php";
+
+  $template = \locate_template($templates, false, false);
+
+  if (empty($template)) {
+    return;
+  }
+
+  // @see load_template (wp-includes/template.php) - these are needed for WordPress to work.
+  global $posts, $post, $wp_did_header, $wp_query, $wp_rewrite, $wpdb, $wp_version, $wp, $id, $comment, $user_ID;
+
+  if (is_array($wp_query->query_vars)) {
+    \extract($wp_query->query_vars, EXTR_SKIP);
+  }
+
+  if (isset($s)) {
+      $s = \esc_attr($s);
+  }
+  // End standard WordPress behavior
+
+  foreach ($namedVariables as $variableName => $value) {
+    if (!preg_match('/^[a-zA-Z_][a-zA-Z0-9_\x7f-\xff]*/', $variableName)) {
+      trigger_error('Variable names must be valid. Skipping "' . $variableName . '" because it is not a valid variable name.');
+      continue;
+    }
+
+    if (isset($$variableName)) {
+      trigger_error("{$variableName} already existed, probably set by WordPress, so it wasn't set to {$value} like you wanted. Instead it is set to: " . print_r($$variableName, true));
+      continue;
+    }
+
+    $$variableName = $value;
+  }
+
+  require $template;
+}
